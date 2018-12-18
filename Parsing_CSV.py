3fdd333f1fea202b84e57e78412351b2ca4bce13
AamirFarooqKhan <45740388+AamirFarooqKhan@users.noreply.github.com>
2018-12-18 19:47:26 +0500

    Add files via upload

--- Parsing_CSV.py
+++ Parsing_CSV.py
@@ -24,8 +24,63 @@ class Parsing_CSV:
                 for keyword in my_parser.get_keywords():
                     csv_generator.writerow([keyword.string,htmlfile_name])
 
+    #Generate Inverted Index
+    def generateInvertedIndex(self):
+       with open("Catalogue.csv", 'r') as keywords_file:     #Opens the already created catalouge file
+           get_forward_index = csv.reader(keywords_file)
+
+           documents = {}   #Dictionary that will store Document:Keywords pair
+
+           for line in get_forward_index:
+               if len(line) > 0:
+                  documents[line[1]] = []   #Creates keys with document names and set it to empty list
+       with open("Catalogue.csv", 'r') as keywords_file:
+           get_forward_index = csv.reader(keywords_file)
+           for line in get_forward_index:
+               if len(line) > 0:
+                   documents[line[1]].append(line[0])   #Creates keyword:documents pair
+
+
+           return documents
+
+    def writeInvertedIndex(self):
+         with open("Inverted_Index.txt",'a') as invert_file:
+           documents = self.generateInvertedIndex()
+           write_inverted = csv.writer(invert_file,delimiter="\t")
+           write_inverted.writerow(["Document Name","Keywords"])
+           for docname in documents:
+               write_inverted.writerow([docname,documents[docname]])
+
+
+    #Generates Forward Index
+    def generateForwardIndex(self):
+        with open("Catalogue.csv", 'r') as keywords_file:   #Opens the already created catalouge file
+            get_forward_index = csv.reader(keywords_file)
+
+            keywords = {}   #Dictionary that will store Keyword:Documents pair
+
+            for line in get_forward_index:
+                if len(line) > 0:
+                    keywords[line[0]] = []  #Creates keys with keywords and set it to empty list
+        with open("Catalogue.csv", 'r') as keywords_file:
+            get_forward_index = csv.reader(keywords_file)
+            for line in get_forward_index:
+                if len(line) > 0:
+                    keywords[line[0]].append(line[1])   #Creates document:keywords pair
+
+            return keywords
+
+    #Write/Appends forward index to txt file.
+    def writeForwardIndex(self):
+        with open("Forward_Index.txt", 'a') as invert_file: #Create new txt file
+            keywords = self.generateForwardIndex()  #Get keywords dictionary
+            write_forward = csv.writer(invert_file, delimiter=">")
+            write_forward.writerow(["Keywords", "Documents"])   #Write keywords:Documents pair to a txt
+            for key in keywords:
+                write_forward.writerow([key, keywords[key]])
 
 
 #TESTING
 start_prog = Parsing_CSV("D:\SearchEngine\Dataset")
-start_prog.storeKeywords()
+start_prog.writeInvertedIndex()
+start_prog.writeForwardIndex()
